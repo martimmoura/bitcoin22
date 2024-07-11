5322265b7974b27e87f6d09235f912880ca4e8a8
Martim Moura <martim.moura@ctw.bmwgroup.com>
2024-07-11 21:46:06 +0100

    typo, trigger cicd

--- infrastructure/cluster/kubeconfig_setup.sh
+++ infrastructure/cluster/kubeconfig_setup.sh
@@ -3,3 +3,7 @@
 
 aws eks list-clusters--region $(terraform output -raw region) update-kubeconfig \
     --name $(terraform output -raw cluster_name) --profile martim
+
+
+aws eks list-clusters--region $(terraform output -raw region) update-kubeconfig \
+    --name $(terraform output -raw cluster_name) --profile martim
