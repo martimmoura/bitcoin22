845a077624bf93c8d488bb9df89a4f9293237242
martimmoura <mlm.martim@hotmail.com>
2024-07-12 00:42:30 +0100

    Feat (#3)
    * added ecr repo, configured k8s objects
    
    * added github action for building image
    
    * changed workflow definition
    
    * manually trigger workflow
    
    * fixed build cmd
    
    * typo
    
    * typo
    
    * added push to ecr step
    
    * added oidc provider for authing from github actions, added auth step to worflow
    
    * push step change
    
    * removed boilerplat workflow
    
    * fixed role to assume for ci
    
    * changes to enable oidc gh auth to iam role
    
    * skip session tagging
    
    * added deploy workflow
    
    * added permission for ci role to generate kubeconfig
    
    * added aws creds reusable worflow to deploy step
    
    * added debug step
    
    * added aws-auth dependency for ci into eks cluster
    
    * typo
    
    * removed test get pods, added k apply manifests
    
    * typo, trigger cicd
    
    * removed debugging steps
    
    * removed debugging steps
    
    * added pvc capacity
    
    * changes to allow for semver tagging of images based on git tags
    
    * skip assuming role if no tag
    
    * readme description
    
    * removed semver stuff, only trigger build on changes to btc22
    
    * trigger ci
    
    * changed pipeline, now versioned image with commit hash
    
    * deleted uneeded files
    
    ---------
    
    Co-authored-by: Martim Moura <martim.moura@ctw.bmwgroup.com>

--- btc22/tag_push_ecr.bash
+++ btc22/tag_push_ecr.bash
@@ -3,8 +3,10 @@ echo logging in to ecr repo
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 982065454085.dkr.ecr.us-east-2.amazonaws.com
 
-echo  tagging image
+echo  tagging image with $1
 
+docker tag btc22 982065454085.dkr.ecr.us-east-2.amazonaws.com/btc22:$1
 docker tag btc22 982065454085.dkr.ecr.us-east-2.amazonaws.com/btc22
 
+docker push 982065454085.dkr.ecr.us-east-2.amazonaws.com/btc22:$1
 docker push 982065454085.dkr.ecr.us-east-2.amazonaws.com/btc22
